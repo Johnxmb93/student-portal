a2ca91deec888e876388987bbb3ce02c98ecaa0b
Marat Oshmiansky <maratical@gmail.com>
2021-08-11 10:57:21 -0700

    say hello again

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App - Marat says hi!" />
+    <HelloWorld msg="Welcome to Your Vue.js App - Marat says hello!" />
   </div>
 </template>
 
