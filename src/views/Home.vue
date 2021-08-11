bd644483900c50cd4fc38c3c1a3bae1367eccfe6
Jonathan Bahena <jonathan.mb93@gmail.com>
2021-08-11 12:57:56 -0500

    added another yo

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App Yo" />
+    <HelloWorld msg="Welcome to Your Vue.js App Yo yo" />
   </div>
 </template>
 
