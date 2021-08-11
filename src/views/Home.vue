b1df013f7fdc3be6550466441f44e6d7aaf497f6
Josh Garcia <joshgarcia7410@gmail.com>
2021-08-11 12:56:03 -0500

    adds edit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App my edit" />
+    <HelloWorld msg="Welcome to Your Vue.js App my edits" />
   </div>
 </template>
 
