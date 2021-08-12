b71f148e58641dff294399f9eb412668ace834fa
Jonathan Bahena <jonathan.mb93@gmail.com>
2021-08-12 11:34:58 -0500

    added login

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,5 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome To The Student Portal" />
+    <h1>Welcome to the Student Portal</h1>
   </div>
 </template>
-
-<script>
-import HelloWorld from "@/components/HelloWorld.vue";
-
-export default {
-  name: "Home",
-  components: {
-    HelloWorld,
-  },
-};
-</script>
